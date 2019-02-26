205be6b68a2c8ef597126d8fecad8fd9fb01048a
Rayan <yangruihan@vip.qq.com>
2019-02-26 17:59:57 +0800

    add position size

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
 // Create a  Pixi Application
 let app = new PIXI.Application({
     width: 400, 
@@ -7,16 +13,31 @@ let app = new PIXI.Application({
     resolution: 1,
 });
 
-app.renderer.backgroundColor = 0xff0000;
-
 // fullscreen set
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.autoResize = true;
-app.renderer.resize(window.innerWidth, window.innerHeight);
+// app.renderer.view.style.position = "absolute";
+// app.renderer.view.style.display = "block";
+// app.renderer.autoResize = true;
+// app.renderer.resize(window.innerWidth, window.innerHeight);
 
 console.log(app.renderer.view.width);
 console.log(app.renderer.view.height);
 
 // Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+// load an image and run the `setup` function when it's done
+loader
+    .add('sprites/c.png')
+    .load(setup);
+
+function setup() {
+    let c = new Sprite(resources['sprites/c.png'].texture);
+
+    c.x = 200;
+    c.y = 150;
+    c.position.set(100, 100);
+    c.width = 64;
+    c.height = 64;
+
+    app.stage.addChild(c);
+}
